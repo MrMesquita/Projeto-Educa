eeb7635db8d67815b48482eea595d4118bc1b62d
Marcelo Guilherme Mesquita <126338859+MrMesquita@users.noreply.github.com>
2023-11-05 12:26:37 -0300

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 $(function(){
-    if(window.screen.width == 375){
-        $('section.sobre > .container > .parte2 h2').html('<h2>Construa seu futuro em uma das melhor escolas de tecnologia do Brasil</h2>');
-        $('footer .chamada-footer .whatsapp span').html('Falar com consultor');
+    var largura = $(window).width();
+    if( largura <= 390){
+        $('section.sobre > .container > .parte2 h2 > br').hide();
+        $('footer .chamada-footer .whatsapp span br').hide();
     }
 })
